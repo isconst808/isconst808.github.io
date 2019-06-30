2c828a869af6ced4961d0def6344a55c175d7373
vtcda <wesale@maillink.top>
2019-06-30 13:09:08 +0300

    Test

---
+++ big-shop/precache-manifest.f96dad1540c59f0f52d87a873bfe1199.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f16b9caac894e291e744c2806b821dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66ce38d5115332bc38a8",
+    "url": "/static/css/main.861affa6.chunk.css"
+  },
+  {
+    "revision": "d3bed2935c767ea88015",
+    "url": "/static/js/2.72d4bfc4.chunk.js"
+  },
+  {
+    "revision": "66ce38d5115332bc38a8",
+    "url": "/static/js/main.3b484945.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
