b10e7b3626258e7758d10b623caed52969a3b99e
Ilya Skryabin <isconst808@gmail.com>
2020-10-30 16:50:34 +0300

    Event load

--- irvas/js/script.js
+++ irvas/js/script.js
@@ -1,32 +1,34 @@
 "use strict";
 
-var TABS_1_LINKS = document.querySelectorAll(".balcony-tab__btn"),
-    TABS_1_BLOCK = document.querySelectorAll('.balcony-tabcontent__col');
-var TABS_2_LINKS = document.querySelectorAll(".decor-tab__label"),
-    TABS_2_BLOCK = document.querySelectorAll('.decor-tabcontent__body');
-TABS_1_LINKS.forEach(function (item) {
-  item.addEventListener("click", function (e) {
-    tabs(e, TABS_1_LINKS, TABS_1_BLOCK, item);
+window.addEventListener('load', function (event) {
+  var TABS_1_LINKS = document.querySelectorAll(".balcony-tab__btn"),
+      TABS_1_BLOCK = document.querySelectorAll('.balcony-tabcontent__col');
+  var TABS_2_LINKS = document.querySelectorAll(".decor-tab__label"),
+      TABS_2_BLOCK = document.querySelectorAll('.decor-tabcontent__body');
+  TABS_1_LINKS.forEach(function (item) {
+    item.addEventListener("click", function (e) {
+      tabs(e, TABS_1_LINKS, TABS_1_BLOCK, item);
+    });
   });
-});
-TABS_2_LINKS.forEach(function (item) {
-  item.addEventListener("click", function (e) {
-    tabs(e, TABS_2_LINKS, TABS_2_BLOCK, item);
+  TABS_2_LINKS.forEach(function (item) {
+    item.addEventListener("click", function (e) {
+      tabs(e, TABS_2_LINKS, TABS_2_BLOCK, item);
+    });
   });
-});
-document.querySelector(".balcony-tab__btn").click();
-document.querySelector(".decor-tab__label").click();
+  document.querySelector(".balcony-tab__btn").click();
+  document.querySelector(".decor-tab__label").click();
 
-function tabs(e, links, block, item) {
-  var activeClassName = 'active';
-  e.preventDefault();
-  var id = e.target.getAttribute("href").replace("#", "");
-  links.forEach(function (child) {
-    child.classList.remove(activeClassName);
-  });
-  block.forEach(function (child) {
-    child.classList.remove(activeClassName);
-  });
-  item.classList.add(activeClassName);
-  document.getElementById(id).classList.add(activeClassName);
-}
+  function tabs(e, links, block, item) {
+    var activeClassName = 'active';
+    e.preventDefault();
+    var id = e.target.getAttribute("href").replace("#", "");
+    links.forEach(function (child) {
+      child.classList.remove(activeClassName);
+    });
+    block.forEach(function (child) {
+      child.classList.remove(activeClassName);
+    });
+    item.classList.add(activeClassName);
+    document.getElementById(id).classList.add(activeClassName);
+  }
+});
